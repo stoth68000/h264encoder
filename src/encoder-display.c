9f8cb22fe6e65588f70ef7fb153d5b1c37dc2c8b
Steven Toth <stoth@localhost.localdomain>
2017-09-05 13:35:47 -0400

    Updated all source/headers to reflect GPL v2 licensing.

--- src/encoder-display.c
+++ src/encoder-display.c
@@ -1,4 +1,27 @@
-/* Copyright 2014 Kernel Labs Inc. All Rights Reserved. */
+/*
+ *  H264 Encoder - Capture YUV, compress via VA-API and stream to RTP.
+ *  Original code base was the vaapi h264encode application, with 
+ *  significant additions to support capture, transform, compress
+ *  and re-containering via libavformat.
+ *
+ *  Copyright (c) 2014-2017 Steven Toth <stoth@kernellabs.com>
+ *  Copyright (c) 2014-2017 Zodiac Inflight Innovations
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <stdio.h>
 #include <string.h>
