b6fb8b7b546eacba1a4450cb2943cf71fe43e16e
Steven Toth <stoth@kernellabs.com>
2017-09-18 11:40:09 -0400

    Debug, something whackey with opt args

--- src/BMDConfig.cpp
+++ src/BMDConfig.cpp
@@ -64,6 +64,7 @@ bool BMDConfig::ParseArguments(int argc,  char** argv)
 
 	while ((ch = getopt(argc, argv, "d:?h3c:s:v:a:m:n:p:t:")) != -1)
 	{
+printf("ch = %c\n", ch);
 		switch (ch)
 		{
 			case 'd':
