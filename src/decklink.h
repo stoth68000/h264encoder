a2a218660b648d91cc84f1409ee8a565fc44484e
Steven Toth <stoth@kernellabs.com>
2017-10-11 09:23:46 -0400

    decklink: whitespace cleanup

--- src/decklink.h
+++ src/decklink.h
@@ -34,10 +34,10 @@ class DeckLinkCaptureDelegate:public IDeckLinkInputCallback {
 public:
 	DeckLinkCaptureDelegate();
 
-	virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid,
-							 LPVOID * ppv) {
+	virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID * ppv) {
 		return E_NOINTERFACE;
-	} virtual ULONG STDMETHODCALLTYPE AddRef(void);
+	}
+	virtual ULONG STDMETHODCALLTYPE AddRef(void);
 	virtual ULONG STDMETHODCALLTYPE Release(void);
 	virtual HRESULT STDMETHODCALLTYPE
 	    VideoInputFormatChanged(BMDVideoInputFormatChangedEvents,
